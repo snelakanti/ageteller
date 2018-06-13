97c7829fdd3ca4eecb8873fdaccb23b1aae86e57
snelakanti <vnelakanti@gmail.com>
2018-06-13 14:46:55 -0400

    import App component to root index.js file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+
+        return(
+            <div>ageteller component</div>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Components/App';
 
 ReactDOM.render(
-<div>ageteller</div>, document.getElementById("root")
+<App />, document.getElementById("root")
 )
 
 
