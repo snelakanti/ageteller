75f031d6d488e0b89da56fcc8a6bb29dec769b12
snelakanti <vnelakanti@gmail.com>
2018-06-13 15:35:30 -0400

    add state to App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,13 +3,25 @@ import {Form, FormControl, Button} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+
+    constructor(){
+    super();
+
+    this.state = {
+        newDate = ""
+    }
+    }
+     
     render(){
 
         return(
             <div className = "App">
                 <Form inline>
                 <h2>Input Your Birthday!</h2>
-                <FormControl type="date">
+                <FormControl 
+                type="date"
+                onChange= {event => this.setState({ newDate: event.target.value })}
+                >
                 </FormControl>
                 {" "}
                 <Button>
